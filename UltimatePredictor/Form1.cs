be5081e050890e32f171bd1cec48d8ea36786bb5
Сергей Гупалов <asterak@gmail.com>
2020-07-01 17:57:01 +0300

    Add project files.

---
+++ UltimatePredictor/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UltimatePredictor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void bPredict_Click(object sender, EventArgs e)
+        {
+           await Task.Run(
+                () =>
+                {
+                    for (int i = 1; i <= 100; i++)
+                    {
+                        this.Invoke(new Action(() =>
+                        {
+                            UpdateProgressBar(i);
+                        }));
+                        Thread.Sleep(100);
+                    }
+
+                }
+            );
+
+            MessageBox.Show("Prediction");
+
+        }
+
+
+        private void UpdateProgressBar(int i)
+        {
+            if (i == progressBar1.Maximum)
+            {
+                progressBar1.Maximum = i + 1;
+                progressBar1.Value = i + 1;
+                progressBar1.Maximum = i;
+            }
+            else
+            {
+                progressBar1.Value = i + 1;
+            }
+            progressBar1.Value = i;
+        }
+
+    }
+}
